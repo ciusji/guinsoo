2f68094c84229644f902f20d56a96192986b52b4
ciusji <bqjimaster@gmail.com>
2021-03-28 21:26:08 +0800

    debug

---
+++ h2/src/test/org/h2/samples/MVStoreUsage.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.h2.samples;
+
+import org.h2.mvstore.MVMap;
+import org.h2.mvstore.MVStore;
+
+/**
+ * MVStoreUsage
+ *
+ * @author cius.ji
+ * @blame guinsoo Group
+ * @since 1.8+
+ */
+public class MVStoreUsage {
+
+    public static void main(String[] args) {
+        // open the store (in-memory if fileName is null)
+        MVStore s = MVStore.open("~/test");
+
+        // create/get the map named "data"
+        MVMap<Integer, String> map = s.openMap("data");
+
+        // add and read some data
+        map.put(5, "Hello World-5");
+        map.put(6, "Hello World-6");
+        map.put(7, "Hello World-7");
+        map.put(8, "Hello World-8");
+
+        System.out.println(map.get(1));
+        System.out.println(map.get(10));
+
+        s.commit();
+
+        // close the store (this will persist changes)
+        s.close();
+    }
+}
