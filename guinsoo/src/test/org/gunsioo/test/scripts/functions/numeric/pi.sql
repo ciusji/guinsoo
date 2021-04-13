df5912c9073641e1fa98ae2b76bc8776e2af47a3
ciusji <bqjimaster@gmail.com>
2021-04-13 19:02:19 +0800

    update package

--- guinsoo/src/test/org/gunsioo/test/scripts/functions/numeric/pi.sql
+++
@@ -1,7 +0,0 @@
--- Copyright 2004-2021 Gunsioo Group. Multiple-Licensed under the MPL 2.0,
--- and the EPL 1.0 (https://github.com/ciusji/guinsoo/blob/master/LICENSE.txt).
--- Initial Developer: Gunsioo Group
---
-
-select pi();
->> 3.141592653589793
