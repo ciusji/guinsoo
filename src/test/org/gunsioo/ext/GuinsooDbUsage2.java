556b663582688736384a67dc28a02853eb00cf9e
ciusji <bqjimaster@gmail.com>
2021-04-21 17:51:06 +0800

    update maven pom

--- src/test/org/gunsioo/ext/GuinsooDbUsage2.java
+++ src/test/org/gunsioo/ext/GuinsooDbUsage2.java
@@ -274,7 +274,7 @@ public class GuinsooDbUsage2 {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         GuinsooDbUsage2 usage = new GuinsooDbUsage2();
         // usage.loadData1();
-        usage.loadData2();
-        // usage.caclData();
+        // usage.loadData2();
+        usage.caclData();
     }
 }
